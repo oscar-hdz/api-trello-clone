3885bec86c622d1ac686f0a8b22c536eeb1ecd8b
Oscar Hernández <ohdz202@gmail.com>
2025-05-18 15:43:11 -0600

    Creando modelos para los usuarios

---
+++ models/model_user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
